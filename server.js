bc1528ec4d2ed86eb5f4d95130f94895382ba13e
Meow <tabbathapischke@gmail.com>
2023-11-08 08:27:41 -1000

    add middlewares

--- server.js
+++ server.js
@@ -22,3 +22,14 @@ const sess = {
       db: sequelize
     })
   };
+
+  app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
