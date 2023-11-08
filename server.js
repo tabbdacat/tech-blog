d9ae01a61bcb8c3da5578ae5355497f079c12fed
Meow <tabbathapischke@gmail.com>
2023-11-08 09:27:58 -1000

    add homepage, fix bugs to run server

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 const sess = {
     secret: 'Super secret secret',
