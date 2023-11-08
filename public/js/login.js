03b65f8582b8b5207ed45cbcaef43324cf97a1c6
Meow <tabbathapischke@gmail.com>
2023-11-08 08:50:13 -1000

    add more query selectors, add login function

--- public/js/login.js
+++ public/js/login.js
@@ -5,5 +5,22 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
 
 
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
     
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert('Failed to log in');
+        }
+      }
+
 };
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
