55d2c9ce6a22a9094ab0bcdb904524262495f9f0
Meow <tabbathapischke@gmail.com>
2023-12-04 14:21:49 -1000

    add comment.js to public/js with front end requests

---
+++ public/js/comment.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const commentBody = document.querySelector('#comment-body').value.trim();
+    const blogId = document.querySelector('#blog-id').value.trim();
+
+    if (commentBody) {
+        console.log('hello')
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ body: commentBody, blog_id: blogId }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace(`/blogs/${blogId}`);
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+        console.log(id)
+      const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to delete comment');
+      }
+    }
+  };
+
+  document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newFormHandler);
+
+  document
+    .querySelector('.comment-list')
+    .addEventListener('click', delButtonHandler);
